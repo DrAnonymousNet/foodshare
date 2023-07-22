a72d6d77630c26fe4ba41626bd0a3d35d423e99a
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-07-22 01:26:56 +0100

    updated Auth app

--- Auth/serializer.go
+++ Auth/serializer.go
@@ -10,27 +10,27 @@ import (
 	core "github.com/DrAnonymousNet/foodshare/Core"
 )
 
-type CreateUserRequest struct{
-	FirstName string 
-	LastName string 
-	FullName string
-	DOB time.Time 
-	Gender string 
-	Email string 
-	Password string 
+type CreateUserRequest struct {
+	FirstName string
+	LastName  string
+	FullName  string
+	DOB       time.Time
+	Gender    string
+	Email     string
+	Password  string
 }
 
-func (c *CreateUserRequest)Bind(r *http.Request) error{
-	if c.Email == ""{
+func (c *CreateUserRequest) Bind(r *http.Request) error {
+	if c.Email == "" {
 		return errors.New("email is required")
 	}
-	
+
 	var user User
 	core.DB.Model(&User{}).Where("Email = ?", c.Email).First(&user)
 	if user.ID != 0 {
 		return errors.New("email alraedy picked")
 	}
-	if c.Password == ""{
+	if c.Password == "" {
 		return errors.New(("invalid password"))
 	}
 	//TODO Validate strong password
@@ -39,5 +39,5 @@ func (c *CreateUserRequest)Bind(r *http.Request) error{
 	c.LastName = strings.ToTitle(c.LastName)
 	c.FullName = c.FirstName + c.LastName
 	return nil
-	
+
 }
