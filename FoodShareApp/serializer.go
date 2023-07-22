6cb54d4242da50a591fb7ee96148f3ece8231c92
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-07-22 22:38:55 +0100

    Added response serializer

--- FoodShareApp/serializer.go
+++ FoodShareApp/serializer.go
@@ -13,18 +13,21 @@ import (
 	"github.com/google/uuid"
 )
 
-
-type DonationHTTPRequestBody struct{
-	UID 		   	uuid.UUID `json:"-"` 
-	Title           string `json:"title" validate:"required"`
-	DonorID         uint8	`json:"donor_id" validate:"required"`
-	DonatedObjType  DonatableObjType `json:"donation_type"`
-	DonationDate    time.Time	`json:"donation_date"`
+type DonationSerializer struct {
+	UID             uuid.UUID          `json:"-"`
+	Title           string             `json:"title" validate:"required"`
+	DonorID         uint8              `json:"donor_id" validate:"required"`
+	DonatedObjType  DonatableObjType   `json:"donation_type"`
+	DonationDate    time.Time          `json:"donation_date"`
 	PickUpAddress   DonationStatusType `json:"pickup_address" validate:"required"`
-	ItemDescription string `json:"item_description"`
+	ItemDescription string             `json:"item_description"`
+	Instance        *Donation          `json:"-"`
+	Instances       []Donation         `json:"-"`
+	// Also note that we only include a pointer to the original Donation struct in our Request struct.
+	// This indirection avoids having to allocate a new copy of Donation.
 }
 
-func (d *DonationHTTPRequestBody)Bind(r *http.Request) error {
+func (d *DonationSerializer) Bind(r *http.Request) error {
 	err := d.Validate(r)
 	if err != nil {
 		return err
@@ -32,7 +35,7 @@ func (d *DonationHTTPRequestBody)Bind(r *http.Request) error {
 	return nil
 }
 
-func (d *DonationHTTPRequestBody)Validate(r *http.Request) error {
+func (d *DonationSerializer) Validate(r *http.Request) error {
 	// Validate the tags in the request body
 	errorMessage := ""
 
@@ -42,18 +45,18 @@ func (d *DonationHTTPRequestBody)Validate(r *http.Request) error {
 		// Return validation errors to the client
 		for _, err := range validationErrors {
 			errorMessage += fmt.Sprintf("Field %s: Validation Error (%s) \n", err.Field(), err.Tag())
-		}	
+		}
 	}
 
 	// Validate Fields in of the struct
-	donationRequestType := reflect.TypeOf(d)
+	donationSerializer := reflect.TypeOf(d)
 
-	if donationRequestType.Kind() != reflect.Struct {
-		for i := 0; i < donationRequestType.NumField(); i++ {
-			field := donationRequestType.Field(i)
+	if donationSerializer.Kind() != reflect.Struct {
+		for i := 0; i < donationSerializer.NumField(); i++ {
+			field := donationSerializer.Field(i)
 			fieldName := field.Name
-			value := reflect.ValueOf(donationRequestType).FieldByName(fieldName)
-			method := reflect.ValueOf(donationRequestType).MethodByName("Validate" + fieldName)
+			value := reflect.ValueOf(donationSerializer).FieldByName(fieldName)
+			method := reflect.ValueOf(donationSerializer).MethodByName("Validate" + fieldName)
 			if method.IsValid() && method.Type().NumIn() == 1 {
 				err := method.Call([]reflect.Value{value})
 				if err != nil {
@@ -67,10 +70,10 @@ func (d *DonationHTTPRequestBody)Validate(r *http.Request) error {
 		return errors.New(errorMessage)
 	}
 	return nil
-	
+
 }
 
-func (d *DonationHTTPRequestBody)ValidateDonorID(ID int) (int, error){
+func (d *DonationSerializer) ValidateDonorID(ID int) (int, error) {
 	var user auth.User
 	core.DB.Model(&auth.User{}).Where("ID = ?", ID)
 	if user.ID == 0 {
@@ -79,9 +82,75 @@ func (d *DonationHTTPRequestBody)ValidateDonorID(ID int) (int, error){
 	return ID, nil
 }
 
-func (d *DonationHTTPRequestBody)ValidateDonation(DonationDate time.Time)(time.Time, error){
-	if d.DonationDate.Before(time.Now()){
+func (d *DonationSerializer) ValidateDonation(DonationDate time.Time) (time.Time, error) {
+	if d.DonationDate.Before(time.Now()) {
 		return time.Time{}, errors.New("donation date cannot be in the past")
 	}
 	return DonationDate, nil
+}
+
+func (d *DonationSerializer) Update() error {
+	donation := reflect.TypeOf(d.Instance)
+	payload := reflect.TypeOf(d)
+	if donation.Kind() != reflect.Struct {
+		for i := 0; i < payload.NumField(); i++ {
+			field := donation.Field(i)
+			fieldName := field.Name
+			value := reflect.ValueOf(payload).FieldByName(fieldName)
+			if value.IsValid() && value.Type().NumIn() == 1 {
+				reflect.ValueOf(donation).FieldByName(fieldName).Set(value)
+			}
+		}
+	}
+	err := core.DB.Model(&Donation{}).Save(&donation).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DonationSerializer) Save(r *http.Request) error {
+	var user auth.User
+	core.DB.Model(&auth.User{}).Where("ID = ?", d.DonorID)
+
+	donationRequest := Donation{
+		UID:             uuid.New(),
+		Title:           d.Title,
+		DonorID:         d.DonorID,
+		DonatedObjType:  d.DonatedObjType,
+		DonationDate:    d.DonationDate,
+		PickUpAddress:   d.PickUpAddress,
+		ItemDescription: d.ItemDescription,
+		User:            user,
+	}
+	err := core.DB.Model(&Donation{}).Create(&donationRequest).Error
+	if err != nil {
+		return err
+	}
+	d.UID = donationRequest.UID
+
+	return nil
+}
+
+func (d *DonationSerializer) Render(w http.ResponseWriter, r *http.Request) error {
+	if d.Instance != nil { //The serializer is only meant to be user for retrieval of donation data
+		donationObj := reflect.TypeOf(d.Instance)
+		donationSerializer := reflect.TypeOf(d)
+		if donationObj.Kind() != reflect.Struct {
+			for i := 0; i < donationSerializer.NumField(); i++ {
+				field := donationObj.Field(i)
+				fieldName := field.Name
+
+				//Get the value of the field in the donation object
+				value := reflect.ValueOf(donationObj).FieldByName(fieldName)
+				if value.IsValid() && value.Type().NumIn() == 1 {
+					//Set the value of the field in the donation
+					reflect.ValueOf(donationSerializer).FieldByName(fieldName).Set(value)
+				}
+			}
+
+		}
+	}
+
+	return nil
 }
