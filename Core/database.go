a7c105661f48c5e111f01a4d800b4b76ca188c95
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-07-22 01:28:41 +0100

    Lint database.go file

--- Core/database.go
+++ Core/database.go
@@ -12,14 +12,13 @@ import (
 var err error
 var DB *gorm.DB // Assume you have a GORM database connection
 
-
-func SetupDatabase() (*gorm.DB, error){
+func SetupDatabase() (*gorm.DB, error) {
 	godotenv.Load(".env")
 
-	dsn :=fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=UTC",
-		 os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("SSL_MODE"))
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=UTC",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("SSL_MODE"))
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil{
+	if err != nil {
 		return DB, err
 	}
 	return DB, nil
