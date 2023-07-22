91e8336d28f9f50fad2a90e640b4397f5e64b7d7
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-07-22 22:37:34 +0100

    Format code

--- FoodShareApp/constants.go
+++ FoodShareApp/constants.go
@@ -2,27 +2,26 @@ package foodshare
 
 type DonatableObjType string
 type DonationStatusType string
-type RequestStatusType string 
+type RequestStatusType string
 type RequestFromType string
 
 const (
-	FoodStuff DonatableObjType = "FoodStuff"
-	Cloths DonatableObjType = "Cloths"
-	MedicalSupplies DonatableObjType ="MedicalSupplies"
-	SchoolSupplies DonatableObjType = "SchoolSupplies"
+	FoodStuff            DonatableObjType = "FoodStuff"
+	Cloths               DonatableObjType = "Cloths"
+	MedicalSupplies      DonatableObjType = "MedicalSupplies"
+	SchoolSupplies       DonatableObjType = "SchoolSupplies"
 	PersonalCareSupplies DonatableObjType = "PersonalCareSupplies"
-	BooksAndToys DonatableObjType = "BooksAndToys"
+	BooksAndToys         DonatableObjType = "BooksAndToys"
 )
 
-
 const (
-	Pending DonationStatusType = "Pending"
+	Pending  DonationStatusType = "Pending"
 	PickedUp DonationStatusType = "PickedUp"
 )
 
 const (
 	PartiallyFulfilled RequestStatusType = "PartiallyFulfilled"
-	FullyFulfilled RequestStatusType = "FullyFulfilled"
+	FullyFulfilled     RequestStatusType = "FullyFulfilled"
 )
 
 const (
--- FoodShareApp/handlers.go
+++ FoodShareApp/handlers.go
@@ -1,2 +1,91 @@
 package foodshare
 
+import (
+	"net/http"
+
+	core "github.com/DrAnonymousNet/foodshare/Core"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+type DonationHandler struct{}
+
+func (d *DonationHandler) CreateDonation(w http.ResponseWriter, r *http.Request) {
+	requestBody := &DonationSerializer{}
+	if err := render.Bind(r, requestBody); err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	err := requestBody.Save(r)
+	if err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	render.Status(r, http.StatusCreated)
+	render.Render(w, r, requestBody)
+
+}
+
+func (d *DonationHandler) GetDonation(w http.ResponseWriter, r *http.Request) {
+	donation_object, err := d.getObject(r)
+	if err != nil {
+		render.Render(w, r, core.ErrNotFound)
+		return
+	}
+	serializer := DonationSerializer{Instance: &donation_object}
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, &serializer)
+}
+
+func (d *DonationHandler) UpdateDonation(w http.ResponseWriter, r *http.Request) {
+	donation_object, err := d.getObject(r)
+	if err != nil {
+		render.Render(w, r, core.ErrNotFound)
+		return
+	}
+	requestBody := &DonationSerializer{Instance: &donation_object}
+	if err := render.Bind(r, requestBody); err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	err = requestBody.Update()
+	if err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, requestBody)
+}
+
+func (d *DonationHandler) DeleteDonation(w http.ResponseWriter, r *http.Request) {
+	donation_object, err := d.getObject(r)
+	if err != nil {
+		render.Render(w, r, core.ErrNotFound)
+		return
+	}
+	err = core.DB.Model(&Donation{}).Delete(&donation_object).Error
+	if err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	render.Status(r, http.StatusNoContent)
+}
+
+func (d *DonationHandler) ListDonations(w http.ResponseWriter, r *http.Request) {
+	var donations []Donation
+	err := core.DB.Model(&Donation{}).Find(&donations).Error
+	if err != nil {
+		render.Render(w, r, core.ErrInvalidRequest(err))
+		return
+	}
+	serializer := DonationSerializer{Instances: donations}
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, &serializer)
+}
+
+func (d *DonationHandler) getObject(r *http.Request) (Donation, error) {
+	uid := chi.URLParam(r, "uid")
+	var donation Donation
+	err := core.DB.Model(&Donation{}).Where("uid = ?", uid).First(&donation).Error
+	return donation, err
+}
